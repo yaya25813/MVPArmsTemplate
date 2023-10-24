05aa905c9709e6751ef2b42333a82863b8ccca30
rwl <ranweilin@mstytech.com>
2023-10-24 17:08:57 +0800

    修改名称

---
+++ src/main/kotlin/other/res/layout/SimpleAdapterXml.kt
@@ -0,0 +1,16 @@
+package other.res.layout
+
+import other.ArmsPluginTemplateProviderImpl
+
+fun simpleAdapterLayout(provider: ArmsPluginTemplateProviderImpl) = """
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:viewBindingIgnore="true"
+    android:orientation="vertical">
+
+</LinearLayout>
+"""
